7dfd165eb21a8e6f19f6c91fa1a6865a5b2f8bdf
Bekzod Ravshanbekov <b.ravshanbekov@mail.ru>
2025-06-27 20:48:48 +0500

    Feat: added create/route.ts & find/route.ts & find/[email]/route.ts

---
+++ src/app/api/users/find/route.ts
@@ -0,0 +1,31 @@
+import { prisma } from '@/lib'
+import { ApiResponse } from '@/lib/api-response'
+
+export async function GET() {
+	try {
+		const existingUser = await prisma.user.findMany()
+
+		if (!existingUser || existingUser.length === 0)
+			return ApiResponse({
+				success: true,
+				message: 'There are no users in database',
+				data: null,
+			})
+
+		const safeUsers = existingUser.map(
+			({ createdAt, password, updatedAt, ...safeUser }) => safeUser
+		)
+
+		return ApiResponse({
+			success: true,
+			message: 'User successfully returned from database',
+			data: safeUsers,
+		})
+	} catch (error) {
+		return ApiResponse({
+			success: false,
+			message: 'Something went wrong in server',
+			data: null,
+		})
+	}
+}
