3148a62ecec2b09fb06c6a21e87a851cb375fd52
Behruz <2310532b@jdu.uz>
2025-11-22 02:39:07 +0500

    feat: enhance mobile navigation by adding visibility control for navbar items

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -5,32 +5,38 @@ export const userNavbar: NavbarMenuType[] = [
 		icon: 'home',
 		title: 'Home',
 		link: '/',
+		isViewInMobile: false,
 	},
 	{
 		icon: 'brain',
 		title: 'Memorize',
 		link: '/memorize',
+		isViewInMobile: true,
 	},
 	{
 		icon: 'badge',
 		title: 'Create Lesson',
 		link: '/create-lesson',
+		isViewInMobile: true,
 	},
 	{
 		icon: 'bookHeart',
 		title: 'My Lessons',
 		link: '/my-lessons',
+		isViewInMobile: true,
 	},
 	{
 		icon: 'reading',
 		title: 'Readings',
 		link: '/readings',
 		badge: 'NEW!',
+		isViewInMobile: true,
 	},
 	{
 		icon: 'settings',
 		title: 'Settings',
 		link: '/settings',
+		isViewInMobile: false,
 	},
 	// {
 	// 	icon: 'FileUser',
@@ -43,12 +49,14 @@ export const userNavbar: NavbarMenuType[] = [
 		title: 'Help',
 		link: '/help',
 		className: 'hover:bg-green-500/30',
+		isViewInMobile: false,
 	},
 	{
 		icon: 'logout',
 		title: 'Logout',
 		link: '/login',
 		className: 'hover:bg-red-500/30',
+		isViewInMobile: false,
 	},
 ]
 export const defaultNavbar: NavbarMenuType[] = [
@@ -56,17 +64,20 @@ export const defaultNavbar: NavbarMenuType[] = [
 		icon: 'home',
 		title: 'Home',
 		link: '/',
+		isViewInMobile: false,
 	},
 	{
 		icon: 'settings',
 		title: 'Settings',
 		link: '/settings',
+		isViewInMobile: false,
 	},
 	{
 		icon: 'help',
 		title: 'Help',
 		link: '/help',
 		className: 'hover:bg-green-500/30',
+		isViewInMobile: false,
 	},
 	// {
 	// 	icon: 'FileUser',
@@ -79,6 +90,7 @@ export const defaultNavbar: NavbarMenuType[] = [
 		title: 'Logout',
 		link: '/login',
 		className: 'hover:bg-red-500/30',
+		isViewInMobile: false,
 	},
 ]
 
@@ -87,26 +99,31 @@ export const AdminNavbar: NavbarMenuType[] = [
 		icon: 'home',
 		title: 'Home',
 		link: '/',
+		isViewInMobile: true,
 	},
 	{
 		icon: 'users',
 		title: 'Users',
 		link: '/users',
+		isViewInMobile: true,
 	},
 	{
 		icon: 'listCheck',
 		title: 'All Lessons',
 		link: '/all-lessons',
+		isViewInMobile: true,
 	},
 	{
 		icon: 'ChartColumnDecreasing',
 		title: 'Diagnostics',
 		link: '/diagnostics',
+		isViewInMobile: true,
 	},
 	{
 		icon: 'settings',
 		title: 'Settings',
 		link: '/settings',
+		isViewInMobile: false,
 	},
 	// {
 	// 	icon: 'FileUser',
@@ -119,5 +136,6 @@ export const AdminNavbar: NavbarMenuType[] = [
 		title: 'Logout',
 		link: '/login',
 		className: 'hover:bg-red-500/30',
+		isViewInMobile: false,
 	},
 ]
